e189c2c7cd39761c0a425df02237d11fcb32eb61
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2021-02-11 11:04:34 -0500

    Add routing comments

--- routes/books.js
+++ routes/books.js
@@ -14,15 +14,19 @@ function asyncHandler(cb) {
     }
 }
 
+
+//GET homepage
 router.get('/', asyncHandler(async (req, res) => {
     const books = await Book.findAll();
     res.render('index', { title: 'Books', books });
 }));
 
+//GET new book form
 router.get('/new', asyncHandler(async (req, res) => {
     res.render('new-book', { title: 'New Book', book: {} });
 })); 
 
+//POST new book to db
 router.post('/', asyncHandler(async (req, res) =>{
     let book;
     try {
@@ -38,6 +42,7 @@ router.post('/', asyncHandler(async (req, res) =>{
     }
 }));
 
+//GET individual book update form
 router.get('/:id', asyncHandler(async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     if (book) {
